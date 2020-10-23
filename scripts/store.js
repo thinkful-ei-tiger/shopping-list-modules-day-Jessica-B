ea511bb26ddab6d7f9a7c05774e1b9b32e7cf1ac
Jessica Baskerville <jmbaskerville1217@gmail.com>
2020-10-23 12:38:21 -0400

    added the findAndDelete function

--- scripts/store.js
+++ scripts/store.js
@@ -1,60 +1,48 @@
+import item from "./item.js";
 
-import item from './item.js';
+let items = [];
+let hideCheckedItems = false;
 
-   let items = [];
-   let  hideCheckedItems = false;
+const findById = function (id) {
+  let foundItem = items.find((item) => item.id === id);
+  return foundItem;
+};
 
- 
-   const findById = function(id){
-    let foundItem = items.find(item => item.id === id);
-        return foundItem;
-      };
+const addItem = function (name) {
+  try {
+    item.validateName(name);
+    this.items.push(item.create(name));
+  } catch (error) {
+    console.log("Error");
+  }
+};
 
-      const addItem = function(name){try{
-        item.validateName(name);
-        this.items.push(item.create(name));}
-        catch(error)
-        {
-            console.log('Error');
-        };
-      };
+const findAndToggleChecked = function (id) {
+  if (this.findById(id).checked === true) {
+    this.findById(id).checked = false;
+  } else this.findById(id).checked = true;
+};
 
-      const findAndToggleChecked = function (id) {
-      if (this.findById(id).checked === true) {
-         this.findById(id).checked = false;
-      }
-      else this.findById(id).checked = true
-      };
+const findAndUpdateName = function (id, newName) {
+  try {
+    item.validateName(newName);
+    findById(id).name = newName;
+  } catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
+};
 
-      const findAndUpdateName = function(id, newName) {
-        try{
-          item.validateName(newName);
-          findById(id).name = newName;
-        }
-        catch('error'){
-          console.log(`Cannot update name: ${error.message}`)
-        };
-      };
+const findAndDelete = function (id) {
+  items.find((item) => item.id === id);
+  items.splice(items, 1);
+};
 
-      function findAndDelete(id) {
-        let deletedArray = this.items.filter(item => item.id !=id);
-        return deletedArray;
-      };
-
-
-
-  export default {
-    items,
-    hideCheckedItems,
-    findById,
-    addItem,
-    findAndToggleChecked,
-    findAndUpdateName,
-    findAndDelete
-  };
-
-
-
-
-  
-  
+export default {
+  items,
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete,
+};
